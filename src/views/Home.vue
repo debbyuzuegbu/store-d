cbeb2a0eefe4416a11a8a4e398165ccf769ed55a
Deborah <debbyuzuegbu@gmail.com>
2023-03-15 02:04:07 +0100

    created a new folder and added all components

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Navbar class="index" />
+    <Dashboard />
+    <Footer />
+  </v-app>
+</template>
+  
+  <script>
+import Navbar from "../components/NavBar";
+import Dashboard from "../components/Dashboard";
+import Footer from "../components/Footer";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  components: {
+    Navbar,
+    Dashboard,
+    Footer,
+  },
+};
+</script>
+  <style scoped>
+.index {
+  z-index: 9999;
+}
+</style>
